014c9fd1fe25f6e31d80ea61853fb0559b303155
Pham Tan Phat <phat.pham@jobhopin.com>
2022-12-17 15:53:34 +0700

    init

---
+++ ngoc-quy/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+
+import Homepage from './pages/homepage';
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
